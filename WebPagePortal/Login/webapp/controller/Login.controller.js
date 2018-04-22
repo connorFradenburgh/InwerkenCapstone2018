7479635c99f426b1a68435dfa25a2dde411d703e
VictorRauta <38056654+VictorRauta@users.noreply.github.com>
2018-04-22 07:12:33 -0500

    Add files via upload

---
+++ WebPagePortal/Login/webapp/controller/Login.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("LoginPageLoginPage.controller.Login", {
+		onClick: function(){
+    	if(this.getView().byId("username").getValue() === "" || this.getView().byId("password").getValue() === "") {
+    	    alert("Please enter a value for every field.");
+    	}
+    	else
+    	{
+    		alert("Login successful! Redirecting to Maintenance Cockpit.");
+    		var loRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			loRouter.navTo("test");
+    	}
+		}
+	});
+});
