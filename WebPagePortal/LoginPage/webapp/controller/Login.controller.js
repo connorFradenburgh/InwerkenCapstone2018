aabcbd529a31086b186a43fcc855160b61ab9fd8
VictorRauta <38056654+VictorRauta@users.noreply.github.com>
2018-04-23 18:58:17 -0500

    Full Integrated Project

---
+++ WebPagePortal/LoginPage/webapp/controller/Login.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("LoginPageLoginPage.controller.Login", {
+		onClick: function(){
+		
+    	if(this.getView().byId("username").getValue() === "" || this.getView().byId("password").getValue() === "") {
+    	    alert("Please enter a value for every field.");
+    	}
+    	else
+    	{
+    		alert("Login successful! Redirecting to Maintenance Cockpit.");
+    		var router = sap.ui.core.UIComponent.getRouterFor(this); 
+			router.navTo("Page1");
+    	}
+		}
+	});
+});
